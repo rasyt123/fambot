0c506f18fd4d0224deeb7f48818221c70637e01c
JustinY <rasyt123@gmail.com>
2021-12-24 14:43:22 -0800

    Update Bishop.h

--- Chess/Bishop.h
+++ Chess/Bishop.h
@@ -4,11 +4,29 @@
 
 #ifndef CHESS_BISHOP_H
 #define CHESS_BISHOP_H
+#include "Pieces.h"
+namespace Chess {
+    class Bishop : Pieces {
+        public:
+        bool GenerateMoves(sf::RenderWindow* window, std::vector<std::vector<char>>& underboard, std::vector<std::vector<Pieces>>& thepieces, std::string color);
+        void grabmovetopleft(std::vector<std::vector<char>>& underboard, std::vector<std::vector<Pieces>>& thepieces, std::string color);
+        void grabmovetopright(std::vector<std::vector<char>>& underboard, std::vector<std::vector<Pieces>>& thepieces, std::string color);
+        void grabmovebottomleft(std::vector<std::vector<char>>& underboard, std::vector<std::vector<Pieces>>& thepieces, std::string color);
+        void grabmovebottomright(std::vector<std::vector<char>>& underboard, std::vector<std::vector<Pieces>>& thepieces, std::string color);
 
 
-class Bishop {
+        private:
+            int startposx;
+            int startposy;
+            int endposx;
+            int endposy;
+            std::vector<std::pair<int,int>> possiblemoves;
 
-};
+
+    };
+
+}
+
 
 
 #endif //CHESS_BISHOP_H
