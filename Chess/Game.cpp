63d17ce547038d85be90c4988a3c1e6765b9171e
JustinY <rasyt123@gmail.com>
2021-12-23 18:33:17 -0800

    Update Game.cpp

--- Chess/Game.cpp
+++ Chess/Game.cpp
@@ -5,10 +5,10 @@
 #include "Game.h"
 
 bool Chess::Game::isEven(int number) {
-    if (number % 2 == 0) 
+    if (number % 2 == 0)
     {
         return true;
-    } else 
+    } else
     {
         return false;
     }
@@ -23,15 +23,15 @@ void Chess::Game::set_cellwidth(int width) {
 }
 
 bool Chess::Game::isPiece(float y, float x, std::pair<float, float>& piececords, std::pair<int, int>& boardcords) {
-    for (int height = 0; height < BOARD_ROWS; height++) 
+    for (int height = 0; height < BOARD_ROWS; height++)
     {
-        for (int width = 0; width < BOARD_COLS; width++) 
+        for (int width = 0; width < BOARD_COLS; width++)
         {
             float cellxpos = width * cell_width;
             float cellypos = height * cell_height;
-            if (x > cellxpos and x < cellxpos + (float) cell_width and y > cellypos and y < cellypos + (float) cell_height) 
+            if (x > cellxpos and x < cellxpos + (float) cell_width and y > cellypos and y < cellypos + (float) cell_height)
             {
-                if (underboard[height][width] != ' ') 
+                if (underboard[height][width] != ' ')
                 {
                     piececords = boardcoords[height][width];
                     boardcords = std::make_pair(height, width);
@@ -44,6 +44,7 @@ bool Chess::Game::isPiece(float y, float x, std::pair<float, float>& piececords,
 }
 
 void Chess::Game::GameLoop() {
+
     sf::RenderWindow window(sf::VideoMode(960, 960), "Chess", sf::Style::Close | sf::Style::Titlebar);
     addcoords();
     bool isgreen = false;
@@ -58,19 +59,19 @@ void Chess::Game::GameLoop() {
     addcoords();
     float clickposy = OUT_OF_BOUNDS;
     float clickposx = OUT_OF_BOUNDS;
-    while (window.isOpen()) 
+    while (window.isOpen())
     {
-        if (isEven(currturncount)) 
+        if (isEven(currturncount))
         {
             currentturn = "white";
-        } else 
+        } else
         {
             currentturn = "black";
         }
         sf::Event event;
-        while (window.pollEvent(event)) 
+        while (window.pollEvent(event))
         {
-            if (event.type == sf::Event::Closed) 
+            if (event.type == sf::Event::Closed)
             {
                 window.close();
                 break;
@@ -78,15 +79,15 @@ void Chess::Game::GameLoop() {
         }
         window.clear();
         SetupBoard(&window);
+
         CheckSelect(&window, isgreen, piececoords, pieceyx, mademove, clickposy, clickposx);
         //Main move doing logic here
         //call makemoveplayer
         window.display();
-        if (mademove) 
+        if (mademove)
         {
             currturncount += 1;
         }
-        isgreen = false;
     }
 }
 
@@ -96,25 +97,34 @@ void Chess::Game::MakeMovePlayer(std::string turn, int posy, int posx) {
 
 
 
+}
+
+
+
+void Chess::Game::HighlightPromotion(sf::RenderWindow *window, Pawn& pawnobj) {
+
+
+
+
+
 
 
 }
 
-
 void Chess::Game::CheckSelect(sf::RenderWindow* window, bool& isgreen,  std::pair<float, float>& piececoords, std::pair<int, int>& pieceyx, bool mademove, float& clickposy, float& clickposx) {
-    if (sf::Mouse::isButtonPressed(sf::Mouse::Left)) 
+    if (sf::Mouse::isButtonPressed(sf::Mouse::Left))
     {
         sf::Vector2i mousePos = sf::Mouse::getPosition(*window);
         clickposy = mousePos.y;
         clickposx = mousePos.x;
-        if (isPiece((float) mousePos.y, (float)mousePos.x, piececoords, pieceyx) and piececoords.first != -9000 and piececoords.second != -9000) 
+        if (isPiece((float) mousePos.y, (float)mousePos.x, piececoords, pieceyx) and piececoords.first != -9000 and piececoords.second != -9000)
         {
-            if (thepieces[pieceyx.first][pieceyx.second].getcolor() == currentturn) 
+            if (thepieces[pieceyx.first][pieceyx.second].getcolor() == currentturn)
             {
                 CoverCellGreen(window, isgreen, piececoords, pieceyx, clickposy, clickposx);
             }
         }
-    } else if (!mademove) 
+    } else if (!mademove)
     {
         CoverCellGreen(window, isgreen, piececoords, pieceyx, clickposy, clickposx);
     }
@@ -123,16 +133,16 @@ void Chess::Game::CheckSelect(sf::RenderWindow* window, bool& isgreen,  std::pai
 
 void Chess::Game::CoverCellGreen(sf::RenderWindow *window, bool &isgreen, std::pair<float, float> &piececoords, std::pair<int, int> &pieceyx, float& clickposy, float& clickposx) {
     if (isPiece(clickposy, clickposx, piececoords, pieceyx) and piececoords.first != -9000 and piececoords.second != -9000
-    and thepieces[pieceyx.first][pieceyx.second].getcolor() == currentturn) 
+    and thepieces[pieceyx.first][pieceyx.second].getcolor() == currentturn)
     {
         sf::RectangleShape boardcell(sf::Vector2f(120.0f, 120.0f));
         boardcell.setPosition(piececoords.first, piececoords.second);
-        if ((isEven(pieceyx.first) and isEven(pieceyx.second)) or (!isEven(pieceyx.first) and !isEven(pieceyx.second))) 
+        if ((isEven(pieceyx.first) and isEven(pieceyx.second)) or (!isEven(pieceyx.first) and !isEven(pieceyx.second)))
         {
             boardcell.setFillColor(sf::Color::White);
             boardcell.setOutlineColor(sf::Color::Green);
             boardcell.setOutlineThickness(-15);
-        } else 
+        } else
         {
             boardcell.setFillColor(sf::Color::Black);
             boardcell.setOutlineColor(sf::Color::Green);
@@ -141,7 +151,6 @@ void Chess::Game::CoverCellGreen(sf::RenderWindow *window, bool &isgreen, std::p
         window->draw(boardcell);
         printPiece(piececoords.first + 26, piececoords.second + 26, pieceyx.first, pieceyx.second, window, thepieces[pieceyx.first][pieceyx.second].getcolor());
     }
-    isgreen = true;
 }
 
 void Chess::Game::SetUnderBoard() {
@@ -161,26 +170,26 @@ void Chess::Game::SetUnderBoard() {
 void Chess::Game::addcoords() {
     std::vector<std::pair<float, float>> rowcoords;
     std::vector<Pieces> piecerows;
-    for (int y = 0; y < BOARD_ROWS; y++) 
-        
-        for (int x = 0; x < BOARD_COLS; x++) 
+    for (int y = 0; y < BOARD_ROWS; y++)
+
+        for (int x = 0; x < BOARD_COLS; x++)
         {
             float cellxpos = x * cell_width;
             float cellypos = y * cell_height;
 
-            if (y >= 0 and y <= 1) 
+            if (y >= 0 and y <= 1)
             {
                 Pieces newpiece;
                 newpiece.settype(underboard[y][x]);
                 newpiece.setcolor("w");
                 piecerows.push_back(newpiece);
-            } else if (y >= 6 and y < BOARD_ROWS) 
+            } else if (y >= 6 and y < BOARD_ROWS)
             {
                 Pieces newpiece;
                 newpiece.settype(underboard[y][x]);
                 newpiece.setcolor("b");
                 piecerows.push_back(newpiece);
-            } else 
+            } else
             {
                 Pieces newpiece;
                 newpiece.setblank(true);
@@ -189,7 +198,7 @@ void Chess::Game::addcoords() {
                 piecerows.push_back(newpiece);
             }
             rowcoords.emplace_back(std::make_pair(cellxpos, cellypos));
-            if (x == BOARD_COLS - 1) 
+            if (x == BOARD_COLS - 1)
             {
                 boardcoords.push_back(rowcoords);
                 rowcoords = {};
@@ -206,7 +215,7 @@ void Chess::Game::printPiece(float spritex, float spritey, int ypos, int xpos, s
     sf::Texture texture;
     bool empty = false;
     imagedir += color;
-    switch (thepieces[ypos][xpos].gettype()) 
+    switch (thepieces[ypos][xpos].gettype())
     {
         case 'P':
             //create pawn subclass here
@@ -243,7 +252,7 @@ void Chess::Game::printPiece(float spritex, float spritey, int ypos, int xpos, s
     }
     sf::Sprite sprite(texture);
     sprite.setPosition(spritex, spritey);
-    if (!empty) 
+    if (!empty)
     {
         window->draw(sprite);
     }
@@ -256,37 +265,37 @@ void Chess::Game::SetupBoard(sf::RenderWindow* window) {
     std::string black = "b";
     std::string white = "w";
     std::string color;
-    for (int y = 0; y < BOARD_ROWS; y++) 
+    for (int y = 0; y < BOARD_ROWS; y++)
     {
-        for (int x = 0; x < BOARD_COLS; x++) 
+        for (int x = 0; x < BOARD_COLS; x++)
         {
             float cellxpos = x * cell_width;
             float cellypos = y * cell_height;
             float spritex = cellxpos + (26);
             float spritey = cellypos + (26);
             sf::Texture texture;
-            if (y <= 1) 
+            if (y <= 1)
             {
                 color = white;
-            } else 
+            } else
             {
                 color = black;
             }
             std::unique_ptr<sf::RectangleShape> boardcell;
             boardcell = std::unique_ptr<sf::RectangleShape>(new sf::RectangleShape(sf::Vector2f(120.0f, 120.0f)));
-            if (isEven(y)) 
+            if (isEven(y))
             {
-                if (isEven(x)) 
+                if (isEven(x))
                 {
                     boardcell->setFillColor(sf::Color::White);
-                } else 
+                } else
                 {
                     boardcell->setFillColor(sf::Color::Black);
                 }
 
-            } else 
+            } else
             {
-               if (isEven(x)) 
+               if (isEven(x))
                {
                    boardcell->setFillColor(sf::Color::Black);
                } else {
