3b47e7a981c2567e1a9259ac2e4982ae5452b2e2
JustinY <rasyt123@gmail.com>
2021-12-24 17:08:15 -0800

    Update Rook.h

--- Chess/Rook.h
+++ Chess/Rook.h
@@ -4,11 +4,29 @@
 
 #ifndef CHESS_ROOK_H
 #define CHESS_ROOK_H
+#include "Pieces.h"
+namespace Chess {
+    class Rook : Pieces {
+        public:
+            std::vector<std::pair<int,int>> getpossiblemoves();
+            std::vector<std::pair<int, int>> getpossiblemovescpy();
 
+            bool GenerateMoves(sf::RenderWindow* window, std::vector<std::vector<char>>& underboard, std::vector<std::vector<Pieces>>& thepieces, std::string color);
+            void gettop(std::vector<std::vector<char>>& underboard, std::vector<std::vector<Pieces>>& thepieces, std::string color, int posy, int posx);
+            void getbottom(std::vector<std::vector<char>>& underboard, std::vector<std::vector<Pieces>>& thepieces, std::string color, int posy, int posx);
+            void getadjacentleft(std::vector<std::vector<char>>& underboard, std::vector<std::vector<Pieces>>& thepieces, std::string color, int posy, int posx);
+            void getadjacentright(std::vector<std::vector<char>>& underboard, std::vector<std::vector<Pieces>>& thepieces, std::string color, int posy, int posx);
 
-class Rook {
-
-};
+        private:
+            bool hasking;
+            int startposx;
+            int startposy;
+            int endposx;
+            int endposy;
+            std::vector<std::pair<int,int>> possiblemoves;
+            std::vector<std::pair<int,int>> possiblemovescpy;
+    };
+}
 
 
 #endif //CHESS_ROOK_H
