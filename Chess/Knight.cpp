54f14fb91144e89ffb2e14245480ae19515bf31d
JustinY <rasyt123@gmail.com>
2021-12-24 21:27:13 -0800

    Update Knight.cpp

--- Chess/Knight.cpp
+++ Chess/Knight.cpp
@@ -17,8 +17,38 @@ std::vector<std::pair<int, int>> Chess::Knight::getpossiblemovescpy() {
 void Chess::Knight::GenerateMoves(std::vector<std::vector<char>>& underboard, std::vector<std::vector<Pieces>>& thepieces, std::string color) {
     std::vector<std::pair<int, int>> possiblekmoves;
     possiblekmoves = {{startposy - 1, startposx - 2}, {startposy - 2, startposx - 1}, {startposy - 2, startposx + 1},
-                      {startposy - 1, startposx + 2}, {startposy + 1, startposx - 2}, {startposy + 2, startposx - 1}, {}};
+                      {startposy - 1, startposx + 2}, {startposy + 1, startposx - 2}, {startposy + 2, startposx - 1}, {startposy + 2, startposx + 1},
+                      {startposy + 1, startposx + 2}};
+    for (std::pair<int, int> horsemove : possiblekmoves)
+    {
+        if (InBounds(horsemove.first, horsemove.second) and underboard[horsemove.first][horsemove.second] and
+        thepieces[horsemove.first][horsemove.second].getcolor() != color)
+        {
+            if ()
+            hasking = true;
 
+        }
+
+    }
+/*
+ *
+ *
+ * if (!InBounds(posy - x, posx - x, underboard) or
+                (underboard[posy - x][posx - x] == 'A' and thepieces[posy - x][posx - x].getcolor() != color))
+        {
+            hasking = true;
+            break;
+        }  else if (underboard[posy - x][posx - x] != ' '
+                    and thepieces[posy - x][posx - x].getcolor() != color)
+        {
+            possiblemoves.emplace_back(std::make_pair(posy - x, posx - x));
+            break;
+        } else
+        {
+            possiblemoves.emplace_back(std::make_pair(posy - x, posx - x));
+        }
+    }
+ */
 
 
 }
