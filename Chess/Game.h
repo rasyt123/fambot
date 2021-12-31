5ffefe08aebe2ef171b1e350250e1fa3d9b5248f
JustinY <rasyt123@gmail.com>
2021-12-31 14:42:36 -0800

    Update Game.h

--- Chess/Game.h
+++ Chess/Game.h
@@ -15,6 +15,7 @@
 #include "Queen.h"
 #include "Knight.h"
 #include "Pieces.h"
+#include "Player.h"
 #include <SFML/Graphics.hpp>
 #define BOARD_ROWS 8
 #define BOARD_COLS 8
@@ -27,10 +28,12 @@ namespace Chess {
         void CheckSelect(sf::RenderWindow* window, bool& isgreen,  std::pair<float, float>& piececoords, std::pair<int, int>& pieceyx, bool mademove, float& clickposy, float& clickposx);
         void CoverCellGreen(sf::RenderWindow *window, bool &isgreen, std::pair<float, float> &piececoords, std::pair<int, int> &pieceyx, float& clickposy, float& clickposx);
         void HighlightPromotion(sf::RenderWindow *window, Pawn& pawnobj, int endposy, int endposx, std::string turn, bool& promotionmove);
-        void MakeMovePlayer(sf::RenderWindow *window, std::string colorturn, std::pair<int, int> startpieceyx, std::pair<int, int> endpieceyx, bool& mademove, bool& checkmate);
+        bool MakeMovePlayer(sf::RenderWindow *window, std::string colorturn, std::pair<int, int> startpieceyx, std::pair<int, int> endpieceyx, bool& mademove, bool& checkmate, int prevy, int prevx, Player& currentplayer);
         bool isTurn(int counter);
-        void Capture(std::vector<std::vector<char>>& underboard, std::vector<std::vector<Pieces>>& thepieces, std::string turn, int startposy, int startposx, int endposy, int endposx);
-        void movetoempty();
+        bool currentlyincheck(sf::RenderWindow *window, std::string colorturn, bool& mademove, int endposy, int endposx, Player currentplayer);
+        bool checkmate(std::string colorturn, int endposy, int endposx);
+        void move(std::vector<std::vector<char>>& underboard, std::vector<std::vector<Pieces>>& thepieces, std::string turn, int startposy, int startposx, int endposy, int endposx, Player& currentplayer);
+        bool IsValidMove(int row, int col, std::vector<std::pair<int, int>> possiblemoves);
         void GameLoop();
         void addcoords();
         void set_cellwidth(int width);
