af342070ce2641b73eb7aae277f9c62110979d7f
JustinY <rasyt123@gmail.com>
2022-02-06 17:41:31 -0800

    Update King.h

--- Chess/King.h
+++ Chess/King.h
@@ -42,7 +42,7 @@ namespace Chess {
         void castlemove(std::vector<std::vector<char>>& underboard, std::vector<std::vector<Pieces>>& thepieces, std::string color);
         void addmoves(std::vector<std::pair<int, int>> src, std::vector<std::pair<int, int>>& destination);
         bool collectmoveinterference(std::vector<std::vector<char>>& underboard, std::vector<std::vector<Pieces>>& thepieces, int y, int x, std::string color);
-        void collectmoveinterference2(std::vector<std::vector<char>>& underboard, std::vector<std::vector<Pieces>>& thepieces, int y, int x, std::string color);
+        void collectmoveinterference2(std::vector<std::vector<char>>& underboard, std::vector<std::vector<Pieces>>& thepieces, int y, int x, std::string color, std::vector<std::pair<int, int>>& addedmoves);
         std::vector<std::pair<int,int>> grabopposingkingssquares(std::vector<std::vector<char>>& underboard, std::vector<std::vector<Pieces>>& thepieces, int y, int x, std::string color);
 
         bool determinestalemate(std::vector<std::vector<char>>& underboard, std::vector<std::vector<Pieces>>& thepieces, std::string color);
@@ -52,6 +52,13 @@ namespace Chess {
         std::pair<int, int> findking(std::string color, std::vector<std::vector<char>>& underboard, std::vector<std::vector<Pieces>>& thepieces);
 
         std::vector<std::pair<int, int>> protectingsquares;
+        std::vector<std::pair<int, int>> totalprotectingsquares;
+
+
+        bool cangobblenearking(int rowpos, int colpos, std::vector<std::vector<char>>& underboard, std::vector<std::vector<Pieces>>& thepieces, std::string color);
+        bool IsPieceProtected(int opppiecerowpos, int opppiececolpos);
+
+
         private:
             bool hasking = false;
             int startposx;
