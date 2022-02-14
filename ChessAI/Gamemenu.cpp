9f54a259f7b78ce235bbc76d9fce22f4756f9585
JustinY <rasyt123@gmail.com>
2022-02-13 17:00:57 -0800

    Update Gamemenu.cpp

--- ChessAI/Gamemenu.cpp
+++ ChessAI/Gamemenu.cpp
@@ -3,3 +3,19 @@
 //
 
 #include "Gamemenu.h"
+
+
+
+void Chess::Gamemenu::loadgamemenu()
+{
+    sf::RenderWindow window(sf::VideoMode(960, 960), "Chess", sf::Style::Close | sf::Style::Titlebar);
+    sf::Texture background;
+
+    while (window.isOpen())
+    {
+        sf::Event event;
+        
+
+
+    }
+}
