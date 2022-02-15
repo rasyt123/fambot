adb04a3667805515bfad87064eb5ad4d4cc3f7ac
JustinY <rasyt123@gmail.com>
2022-02-15 00:49:27 -0800

    Update Gamemenu.cpp

--- ChessAI/Gamemenu.cpp
+++ ChessAI/Gamemenu.cpp
@@ -5,29 +5,83 @@
 #include "Gamemenu.h"
 
 
+void Chess::Gamemenu::addfonts(sf::RenderWindow *window) {
+    sf::Text Play;
+    sf::Text Options;
+    sf::Text Quit;
+    sf::Font font;
 
-void Chess::Gamemenu::loadgamemenu()
+    Play.setString("Play");
+    Options.setString("Options");
+    Quit.setString("Quit");
+
+
+    Play.setFont(font);
+    Options.setFont(font);
+    Quit.setFont(font);
+
+
+    Play.setCharacterSize(24);
+    Options.setCharacterSize(24);
+    Quit.setCharacterSize(24);
+
+    Play.setFillColor(sf::Color(0, 0, 0));
+    Options.setFillColor(sf::Color());
+    Quit.setFillColor(sf::Color());
+
+
+    Play.setStyle(sf::Text::Bold);
+    Options.setStyle(sf::Text::Bold);
+    Quit.setStyle(sf::Text::Bold);
+    
+}
+
+bool Chess::Gamemenu::playbuttonpressed(float posx, float posy) {
+
+
+}
+
+
+bool Chess::Gamemenu::optionsbuttonpressed(float posx, float posy) {
+
+
+
+
+}
+
+bool Chess::Gamemenu::quitbuttonpressed(float posx, float posy) {
+
+
+
+
+}
+
+
+bool Chess::Gamemenu::loadgamemenu(sf::RenderWindow *window)
 {
-    sf::RenderWindow window(sf::VideoMode(960, 960), "Chess", sf::Style::Close | sf::Style::Titlebar);
     sf::Texture background;
     sf::Sprite backsprite;
-    background.loadFromFile("C:\\Users\\rasyt\\Pictures\\Saved Pictures\\chessui.png");
+    background.loadFromFile("C:\\Users\\rasyt\\Pictures\\Saved Pictures\\chessui.jpg");
     backsprite.setTexture(background);
-    backsprite.setScale(960/1200, 960/800);
+    //backsprite.setScale(960/1200, 960/800);
+    backsprite.setPosition(0, 0);
 
-    sf::RectangleShape PlayButton(sf::Vector2f(420.0f, 250.0f));
+    window->draw(backsprite);
+    sf::RectangleShape PlayButton(sf::Vector2f(420.0f, 150.0f));
     PlayButton.setFillColor(sf::Color(255, 255, 255));
 
-    sf::RectangleShape OptionsButton(sf::Vector2f(420.0f, 250.0f));
+    sf::RectangleShape OptionsButton(sf::Vector2f(420.0f, 150.0f));
     OptionsButton.setFillColor(sf::Color(255, 255, 255));
 
-    sf::RectangleShape Quitbutton(sf::Vector2f(420.0f, 250.0f));
+    sf::RectangleShape Quitbutton(sf::Vector2f(420.0f, 150.0f));
     Quitbutton.setFillColor(sf::Color(255, 255, 255));
 
-    PlayButton.setPosition();
-    OptionsButton.setPosition();
-    Quitbutton.setPosition();
+    PlayButton.setPosition(250, 150);
+    OptionsButton.setPosition(250, 400);
+    Quitbutton.setPosition(250, 650);
 
-
-
+    window->draw(PlayButton);
+    window->draw(OptionsButton);
+    window->draw(Quitbutton);
+    return false;
 }
