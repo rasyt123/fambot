98e957407dd45e3e97f9c7b6651d84a0b5ca927b
JustinY <rasyt123@gmail.com>
2022-02-21 13:26:00 -0800

    Update Gamemenu.h

--- ChessAI/Gamemenu.h
+++ ChessAI/Gamemenu.h
@@ -14,18 +14,36 @@ namespace Chess {
     public:
 
         void addfonts(sf::RenderWindow *window);
-        bool loadgamemenu(sf::RenderWindow *window);
+        void addmenu2fonts(sf::RenderWindow *window, std::string topbuttonstr, std::string midbuttonstr, std::string bottomstr);
+        bool loadgamemenu(sf::RenderWindow *window, float highlightx, float highlighty);
+        void loadplaymenu(sf::RenderWindow *window, float highlightx, float highlighty);
 
-        bool playbuttonpressed(float posx, float posy);
-        bool quitbuttonpressed(float posx, float posy);
-        bool optionsbuttonpressed(float posx, float posy);
+        bool highlightplay(float posx, float posy);
+        bool highlightquit(float posx, float posy);
+        bool highlightoptions(float posx, float posy);
         void windowpoller(sf::RenderWindow *window);
         void checkmatemenu(sf::RenderWindow *window);
+        void parseclicks(sf::RenderWindow *window);
+        void parseclicks2(sf::RenderWindow *window);
+
+        void createbuttons(sf::RenderWindow *window, float highlightx, float highlighty);
+        void checkmatemenu(sf::RenderWindow *window, std::string color);
+
+
+        bool geteasyai();
+        bool getmediumai();
+        bool gethumanopponent();
+        bool getplaybutton();
 
 
 
     private:
         std::vector<std::pair<int,int>> menubarcoords;
+        bool easyai = false;
+        bool mediumai = false;
+        bool humanopponent = false;
+        bool playbutton = false;
+        bool checkmate = false;
 
 
 
