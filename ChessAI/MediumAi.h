0d802bc802f9bac2e21fa825bc8bb51f31ddddb5
JustinY <rasyt123@gmail.com>
2022-03-03 16:43:02 -0800

    Update MediumAi.h

--- ChessAI/MediumAi.h
+++ ChessAI/MediumAi.h
@@ -5,10 +5,48 @@
 #ifndef CHESS_MEDIUMAI_H
 #define CHESS_MEDIUMAI_H
 
+#include <vector>
 
-class MediumAi {
+namespace Chess {
+    class MediumAi {
 
-};
+
+
+    public:
+        void setPawnevalsquares();
+        void setKnightevalsquares();
+        void setBishopevalsquares();
+        void setRookevalsquares();
+        void setQueenevalsquares();
+        void setKingevalsquares();
+
+
+
+    private:
+        //Eval Function
+        //Square Piece Tables
+        std::vector<std::vector<int>> pawnevalsquares;
+        std::vector<std::vector<int>> knightevalsquares;
+        std::vector<std::vector<int>> bishopevalsquares;
+        std::vector<std::vector<int>> rookevalsquares;
+        std::vector<std::vector<int>> queenevalsquares;
+        std::vector<std::vector<int>> kingevalsquares;
+
+
+
+
+
+
+
+
+    };
+
+
+
+
+
+
+}
 
 
 #endif //CHESS_MEDIUMAI_H
